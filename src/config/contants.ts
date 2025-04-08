560465a736705eae56bee19e03ec76b013a582f4
Ana Toader <ana_toader81@yahoo.com>
2025-04-09 00:12:21 +0300

    Fix hostname
    Signed-off-by: Ana Toader <ana_toader81@yahoo.com>

--- src/config/contants.ts
+++ src/config/contants.ts
@@ -1,2 +1,2 @@
-export const APP_ADDRESS = `${process.env.HOST}:${process.env.PORT}`;
+export const APP_ADDRESS = `${process.env.HOSTNAME}:${process.env.PORT}`;
 export const REDIRECT_ADDRESS = process.env.REDIRECT_URL ?? APP_ADDRESS;
